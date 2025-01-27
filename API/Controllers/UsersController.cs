f091465936315ee6fd1d32ef47202518cfe8e6c0
Mikel Jimenez <majc1907@gmail.com>
2025-01-27 13:14:15 +0100

    crud operations ok

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -28,5 +28,37 @@ public class UsersController(DataContext context) : ControllerBase
         return user;
     }
 
+    [HttpPost]
+    public async Task<ActionResult<AppUser>> PostUser(AppUser user)
+    {
+        await context.Users.AddAsync(user);
+        await context.SaveChangesAsync();
+        return CreatedAtAction(nameof(PostUser), new{id= user.Id}, user );
+    }
 
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<AppUser>> DeleteUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if(user == null){
+            return NotFound();
+        }
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        Console.WriteLine($"User {id} removed succesfully"); 
+        return NoContent();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<AppUser>> UpdateUser(int id, AppUser user)
+    {
+        var existingUser = await context.Users.FindAsync(id);
+        if(existingUser == null){
+            return NotFound();
+        }
+        existingUser.UserName = user.UserName;
+        await context.SaveChangesAsync();
+        return Ok(existingUser);
+    }
 }
