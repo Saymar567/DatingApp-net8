6fa6f109cd3008a19fce5684e70dad3d7ad36545
Mikel Jimenez <majc1907@gmail.com>
2025-02-11 13:28:54 +0100

    nineth-commit

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -5,10 +5,11 @@ namespace API.DTOs;
 
 public class RegisterDTO
 {
- [Required]
- [MaxLength(100)]
-    public required string UserName {get; set; }
-    
-   [Required] 
-    public required string Password {get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(12, MinimumLength = 8)]
+    public string Password { get; set; } = string.Empty;
 }
